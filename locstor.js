cb0ddda9559779e89936fb2c612d5fea841e2a70
Justin de Guzman <justin@justindeguzman.net>
2013-08-08 04:28:15 -0700

    placed functions inside scope wrapper

--- locstor.js
+++ locstor.js
@@ -1,7 +1,7 @@
 /**
  * Locstor.js is a JavaScript helper library for HTML5 localStorage.
  *
- * v1.0.4
+ * v1.0.5
  *
  * http://locstorjs.com
  *
@@ -9,122 +9,17 @@
  * Released under the MIT license.
  */
 
-function Locstor() {}
-// Private Methods
-// ---------------
+var Locstor = function Locstor() {};
 
-// Inital check to see if localStorage is supported in the browser
 (function() {
-	var supported = false;
+	// Private Properties
+	// -----------------
 
-	// Derived from Modernizer (http://github.com/Modernizr/Modernizr)
-	try {
-		localStorage.setItem('test', 'test');
-		localStorage.removeItem('test');
-		supported = true;
-	} catch(e) {
-		supported = false;
-	}
-
-	// Implements localStorage if not supported
-	// From https://developer.mozilla.org/en-US/docs/Web/Guide/DOM/Storage?redirectlocale=en-US&redirectslug=DOM%2FStorage
-	if(!supported) {
-		window.localStorage = {
-			getItem: function (sKey) {
-				if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
-				return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
-					"\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
-			},
-
-			key: function (nKeyId) {
-				return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
-			},
-
-			setItem: function (sKey, sValue) {
-				if(!sKey) { return; }
-				document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
-				this.length = document.cookie.match(/\=/g).length;
-			},
-
-			length: 0,
-
-			removeItem: function (sKey) {
-				if (!sKey || !this.hasOwnProperty(sKey)) { return; }
-				document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-				this.length--;
-			},
-
-			hasOwnProperty: function (sKey) {
-				return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-			}
-		};
-
-		window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
-	}
-})();
-
-// Public Methods
-// --------------
-
-// Removes all key/value pairs from localStorage
-Locstor.clear = function clear() {
-	localStorage.clear();
-};
-
-// Checks if localStorage contains the specified key
-Locstor.contains = function contains(key) {
-	if(typeof key != 'string') {
-		throw new Error('Key must be a string for function contains(key)');
-	}
-
-	return this.getKeys().indexOf(key) !== -1;
-};
-
-// Returns the value of a specified key in localStorage
-// The value is converted to the proper type upon retrieval 
-Locstor.get = function get(key) {
-	if(typeof key != 'string') {
-		throw new Error('Key must be a string for function get(key)');
-	}
-
-	var value = localStorage.getItem(key);	// retrieve value
-	var number = parseFloat(value);	// to allow for number checking
-
-	if(!isNaN(number)) {
-		return number;	// value was of type number
-	} else if(value.toLowerCase() === 'true' || value.toLowerCase() === 'false') {
-		return value === 'true';	//value was of type boolean
-	} else if(value === 'null') {
-		return null;	// value was null
-	} else {
-		try {
-			value = JSON.parse(value);
-			return value;
-		} catch(e) {
-			return value;
-		}
-	}
-};
-
-// Returns an array of keys currently stored in localStorage
-Locstor.getKeys = function getKeys() {
-	var result = [];
-	
-	for(var i = 0; i < localStorage.length; i++) {
-		result.push(localStorage.key(i));
-	}
-
-	return result;
-};
-
-// Returns how much space is left in localStorage
-// The most common limit for browsers today is 5MB, as specified by default size
-// IE8+ has a properly implemented remainingSpace property which is used if
-// possible, otherwise it is approximated by subtracting the defaultSize with
-// the total size of all the strings in localStorage
-Locstor.getRemainingSpace = function getRemainingSpace() {
+	// Common 5MB localStorage limit among current browsers
 	var defaultSize = 5242880;
 
+	// Private Methods
+	// ---------------
 	// IE detection method courtesy of James Padolsey
 	// http://james.padolsey.com/javascript/detect-ie-in-js-using-conditional-comments
 	var ie = (function(){
@@ -137,74 +32,184 @@ Locstor.getRemainingSpace = function getRemainingSpace() {
 		return v > 4 ? v : undef;
 	}());
 
-	var result = ie && ie > 7 ? localStorage.remainingSpace : defaultSize - this.getSize();
-	return result;
-};
+	// Inital check to see if localStorage is supported in the browser
+	(function() {
+		var supported = false;
 
-// Returns the size of the total contents in localStorage 
-Locstor.getSize = function getSize() {
-	return JSON.stringify(localStorage).length;
-};
+		// Derived from Modernizer (http://github.com/Modernizr/Modernizr)
+		try {
+			localStorage.setItem('test', 'test');
+			localStorage.removeItem('test');
+			supported = true;
+		} catch(e) {
+			supported = false;
+		}
 
-// Returns true if localStorage has no key/value pairs
-Locstor.isEmpty = function isEmpty() {
-	return this.getKeys().length === 0;
-};
+		// Implements localStorage if not supported
+		// From https://developer.mozilla.org/en-US/docs/Web/Guide/DOM/Storage?redirectlocale=en-US&redirectslug=DOM%2FStorage
+		if(!supported) {
+			window.localStorage = {
+				getItem: function (sKey) {
+					if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+					return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
+						"\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+				},
 
-// Removes the specified key/value pair from localStorage given a key
-// Optionally takes an array to remove key/value pairs specified in the array
-Locstor.remove = function remove(key) {
-	if(typeof key == 'string') {
-		localStorage.removeItem(key);
-	} else if(key instanceof Array) {
-		for(var i = 0; i < key.length; i++) {
-			if(typeof key[i] == 'string') {
-				localStorage.removeItem(key[i]);
-			} else {
-				throw new Error('Key in index ' + i + ' is not a string');
+				key: function (nKeyId) {
+					return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+				},
+
+				setItem: function (sKey, sValue) {
+					if(!sKey) { return; }
+					document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+					this.length = document.cookie.match(/\=/g).length;
+				},
+
+				length: 0,
+
+				removeItem: function (sKey) {
+					if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+					document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+					this.length--;
+				},
+
+				hasOwnProperty: function (sKey) {
+					return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+				}
+			};
+
+			window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+		}
+	})();
+
+	// Public Methods
+	// --------------
+
+	// Removes all key/value pairs from localStorage
+	Locstor.clear = function clear() {
+		localStorage.clear();
+	};
+
+	// Checks if localStorage contains the specified key
+	Locstor.contains = function contains(key) {
+		if(typeof key != 'string') {
+			throw new Error('Key must be a string for function contains(key)');
+		}
+
+		return this.getKeys().indexOf(key) !== -1;
+	};
+
+	// Returns the value of a specified key in localStorage
+	// The value is converted to the proper type upon retrieval 
+	Locstor.get = function get(key) {
+		if(typeof key != 'string') {
+			throw new Error('Key must be a string for function get(key)');
+		}
+
+		var value = localStorage.getItem(key);	// retrieve value
+		var number = parseFloat(value);	// to allow for number checking
+
+		if(!isNaN(number)) {
+			return number;	// value was of type number
+		} else if(value.toLowerCase() === 'true' || value.toLowerCase() === 'false') {
+			return value === 'true';	//value was of type boolean
+		} else if(value === 'null') {
+			return null;	// value was null
+		} else {
+			try {
+				value = JSON.parse(value);
+				return value;
+			} catch(e) {
+				return value;
 			}
 		}
-	} else {
-		throw new Error('Key must be a string or array for function remove(key || array)');
-	}
-};
+	};
 
-// Stores the specified key value pair (allows string, number, boolean, object, array)
-// If no key and only an object is passed, this function acts as an alias function for store(object)
-Locstor.set = function set(key, value) {
-	if(arguments.length == 1) {
-		this.store(key);
-	} else if(typeof key == 'string') {
-		if(typeof value === 'object') {
-			value = JSON.stringify(value);
+	// Returns an array of keys currently stored in localStorage
+	Locstor.getKeys = function getKeys() {
+		var result = [];
+		
+		for(var i = 0; i < localStorage.length; i++) {
+			result.push(localStorage.key(i));
 		}
 
-		localStorage.setItem(key, value);
-	} else {
-		throw new Error('Invalid arguments for function set(key, value) or function set(object)');
-	}
-};
+		return result;
+	};
 
-// Stores the object in localStorage, allowing access to individual object properties
-Locstor.store = function store(value) {
-	if(typeof value == 'object' && !(value instanceof Array)) {
-		for(var property in value) {
-			localStorage.setItem(property, value[property]);
+	// Returns how much space is left in localStorage
+	// The most common limit for browsers today is 5MB, as specified by default size
+	// IE8+ has a properly implemented remainingSpace property which is used if
+	// possible, otherwise it is approximated by subtracting the defaultSize with
+	// the total size of all the strings in localStorage
+	Locstor.getRemainingSpace = function getRemainingSpace() {
+		return ie && ie > 7 ? localStorage.remainingSpace : defaultSize - this.getSize();
+	};
+
+	// Returns the size of the total contents in localStorage 
+	Locstor.getSize = function getSize() {
+		return JSON.stringify(localStorage).length;
+	};
+
+	// Returns true if localStorage has no key/value pairs
+	Locstor.isEmpty = function isEmpty() {
+		return this.getKeys().length === 0;
+	};
+
+	// Removes the specified key/value pair from localStorage given a key
+	// Optionally takes an array to remove key/value pairs specified in the array
+	Locstor.remove = function remove(key) {
+		if(typeof key == 'string') {
+			localStorage.removeItem(key);
+		} else if(key instanceof Array) {
+			for(var i = 0; i < key.length; i++) {
+				if(typeof key[i] == 'string') {
+					localStorage.removeItem(key[i]);
+				} else {
+					throw new Error('Key in index ' + i + ' is not a string');
+				}
+			}
+		} else {
+			throw new Error('Key must be a string or array for function remove(key || array)');
 		}
-	} else {
-		throw new Error('Argument for function set(object) must be an object');
-	}
-};
+	};
 
-// Returns an object representation of the current state of localStorage
-Locstor.toObject = function toObject() {
-	var o = {};
-	var keys = this.getKeys();
+	// Stores the specified key value pair (allows string, number, boolean, object, array)
+	// If no key and only an object is passed, this function acts as an alias function for store(object)
+	Locstor.set = function set(key, value) {
+		if(arguments.length == 1) {
+			this.store(key);
+		} else if(typeof key == 'string') {
+			if(typeof value === 'object') {
+				value = JSON.stringify(value);
+			}
 
-	for(var i = 0; i < keys.length; i++) {
-		var key = keys[i];
-		o[key] = this.get(key);
-	}
+			localStorage.setItem(key, value);
+		} else {
+			throw new Error('Invalid arguments for function set(key, value) or function set(object)');
+		}
+	};
 
-	return o;
-};
+	// Stores the object in localStorage, allowing access to individual object properties
+	Locstor.store = function store(value) {
+		if(typeof value == 'object' && !(value instanceof Array)) {
+			for(var property in value) {
+				localStorage.setItem(property, value[property]);
+			}
+		} else {
+			throw new Error('Argument for function set(object) must be an object');
+		}
+	};
+
+	// Returns an object representation of the current state of localStorage
+	Locstor.toObject = function toObject() {
+		var o = {};
+		var keys = this.getKeys();
+
+		for(var i = 0; i < keys.length; i++) {
+			var key = keys[i];
+			o[key] = this.get(key);
+		}
+
+		return o;
+	};
+})();
